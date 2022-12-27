2177eff1d62af5b115eb732703b17dcbfa1df629
Ioan <121471930+ioaM7@users.noreply.github.com>
2022-12-27 14:16:29 -0300

    Add files via upload

---
+++ ParImpar.py
@@ -0,0 +1,10 @@
+
+
+print("Hola, y bienvenidos a esta prueba.."
+      " A continuación ingresa un número entre 1 y 1000")
+num1=int(input("¿En que numero estas pensando?: "))
+if num1 %2 == 0:
+    print(num1, "¡Es un numero par!")
+else:
+    print(num1, "¡Es un numero impar!, ¿Queres añadir otro?")
+
