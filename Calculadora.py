68ef979c905c02cef78d1a5a1c61c03c4b21160f
Ioan <121471930+ioaM7@users.noreply.github.com>
2023-01-10 17:57:05 -0300

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,6 +1,7 @@
-# Realizar un programa en el cual se declaren dos valores enteros por teclado utilizando
-# el método __init__. Calcular después la suma, resta, multiplicación y división.
-# Utilizar un método para cada una e imprimir los resultados obtenidos. Llamar a la clase Calculadora.
+# Realizar un programa en el cual se declaren dos valores enteros por teclado utilizando el método __init__.
+# Calcular después la suma, resta, multiplicación y división.
+# Utilizar un método para cada una e imprimir los resultados obtenidos. 
+# Llamar a la clase Calculadora.
 
 class Calculadora():
     num1=float(input("ingrese un numero entero: "))
